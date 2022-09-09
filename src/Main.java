2e18d8da0aec2bfa35d1cb5d168cee333ad3418f
lizhicheng <lizhicheng@wudi.xyz>
2022-09-09 11:51:08 +0800

    init

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String res = s.longestPalindrome();
+        System.out.println(res);
+
+
+    }
+}
+
+
